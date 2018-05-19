07cac30a6450b07d33b35a2b310088cb7e6e7f38
edgarpng <edgar.png@gmail.com>
2018-05-19 13:41:37 -0700

    Add skeletons for endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,30 @@
 var appRouter = function (app) {
-  app.get("/", function(req, res) {
-    res.status(200).send("Welcome to our restful API");
+  app.get("/cache/:key", function(req, res) {
+    var key = req.params.key;
+    res.status(200).send("Getting key: " + key);
+  });
+
+  app.get("/cache", function(req, res) {
+    //TODO
+    res.status(200).send("Getting all keys");
+  });
+
+  app.post("/cache/:key", function(req, res) {
+    //TODO
+    var key = req.params.key;
+    var value = req.body.value;
+    res.status(200).send("Setting value of key " + key + " to " + value);
+  });
+
+  app.delete("/cache/:key", function(req, res) {
+    //TODO
+    var key = req.params.key;
+    res.status(200).send("Deleting value for key " + key);
+  });
+
+  app.delete("/cache", function(req, res) {
+    //TODO
+    res.status(200).send("Deleting every key");
   });
 }
 
